533a2021f919990099c9ef592495603841b6a848
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2025-12-31 22:25:37 +0200

    try to match the user role id

--- telegraf/entrypoint.sh
+++ telegraf/entrypoint.sh
@@ -18,11 +18,23 @@ if [ -e /var/run/docker.sock ]; then
     usermod -aG "$TARGET_GROUP" telegraf
 fi
 
-# Execute the original entrypoint or command
-# The official Telegraf image uses /entrypoint.sh to drop privileges
-if [ -f /entrypoint.sh ]; then
-    exec /entrypoint.sh "$@"
+    echo "Adding telegraf user to group '$TARGET_GROUP'"
+    usermod -aG "$TARGET_GROUP" telegraf
+fi
+
+# We are running as root, so we need to manually drop privileges to 'telegraf'
+# effectively after updating the groups.
+# Note: standard /entrypoint.sh might reset or ignore our group changes if it uses setpriv strictly.
+
+if [ "$1" = "telegraf" ]; then
+    # We use 'setpriv' directly if available (standard in telegraf image) 
+    # OR we can just run the command as the user.
+    # The official image uses 'setpriv --reuid telegraf --init-groups ...' which MIGHT wipe our dynamic group.
+    # So we should run the command directly as telegraf, preserving the new group.
+    
+    # Switch to telegraf user preserving the new supplementary groups
+    exec su telegraf -c "$*"
 else
-    # Fallback if original entrypoint doesn't exist
+    # Fallback for other commands
     exec "$@"
 fi
